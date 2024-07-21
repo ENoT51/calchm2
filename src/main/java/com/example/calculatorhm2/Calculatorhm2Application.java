ac484d46479a69882d852332cf9cac6a26aac378
ENoT51 <ambarov515@gmail.com>
2024-07-21 19:06:50 +0300

    initial commit

---
+++ src/main/java/com/example/calculatorhm2/Calculatorhm2Application.java
@@ -0,0 +1,13 @@
+package com.example.calculatorhm2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculatorhm2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculatorhm2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/calculatorhm2/Calculatorhm2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculatorhm2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculatorhm2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
